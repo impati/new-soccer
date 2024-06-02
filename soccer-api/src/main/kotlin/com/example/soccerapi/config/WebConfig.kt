8e8bec0632ff52c5e6bc1b81f4e90ee828020fbf
impati <yongs170@naver.com>
2024-06-02 15:01:08 +0900

    fix: cors 설정

---
+++ soccer-api/src/main/kotlin/com/example/soccerapi/config/WebConfig.kt
@@ -0,0 +1,23 @@
+package com.example.soccerapi.config
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+
+@Configuration
+class WebConfig {
+
+
+    @Bean
+    fun corsConfigurer(): WebMvcConfigurer {
+        return object : WebMvcConfigurer {
+            override fun addCorsMappings(registry: CorsRegistry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:4000")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+            }
+        }
+    }
+}
